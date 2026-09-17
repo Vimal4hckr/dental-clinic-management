2ee59bd36f5793675b457d8df33d2598e9a43c0d
Vimal4hckr <152588656+Vimal4hckr@users.noreply.github.com>
2026-09-17 10:13:11 +0530

    Redesign clinic branding and navbar

--- dental_clinic/settings.py
+++ dental_clinic/settings.py
@@ -32,9 +32,9 @@ INSTALLED_APPS = [
     'aiassistant',
     'manage_panel',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,12 +87,12 @@ USE_TZ = True
 
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user uploads)
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
